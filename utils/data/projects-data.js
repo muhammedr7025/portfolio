62a9aded091486358b173b8afca719aff207c5e5
Muhammed R <64767974+muhammedr7025@users.noreply.github.com>
2025-07-29 14:05:27 +0530

    Update projects-data.js

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -2,9 +2,28 @@ import crefin from '/public/image/crefin.jpg';
 import realEstate from '/public/image/real-estate.jpg';
 import travel from '/public/image/travel.jpg';
 
-export const projectsData = [
-    {
+export const projectsData = [{
         id: 1,
+        name: 'kriyado',
+        description: 'Kriyado is a collaborative space where customers and vendors come together.',
+        tools: ['flutter','Django', 'MySql', 'JWT','firebase'],
+        code: '',
+        role: 'Software Developer',
+        demo: 'https://kriyado.com/',
+        image: realEstate,
+    },
+    {
+        id: 2,
+        name: 'Let Me Goo',
+        description: '',
+        tools: ['flutter','Django', 'MySql', 'JWT','firebase'],       
+        code: '',
+        role: 'Software Developer',
+        demo: 'https://letmegoapp.com/',
+        image: realEstate,
+    },
+    {
+        id: 3,
         name: 'Aura Robo',
         description: "This project involves developing a versatile home automation robot designed to enhance everyday living. Controlled via a mobile app, the robot can move around the home, interact with objects, and perform tasks such as automatic lighting and vacuum cleaning. Additionally, it features live video streaming capabilities, allowing users to monitor their home in real time. This innovative solution combines convenience and security, making home management easier and more efficient.",
         tools: ['Arduino IDE', 'Firebase', 'Flutter','AWS'],
@@ -14,7 +33,7 @@ export const projectsData = [
         image: crefin,
     },
     {
-        id: 2,
+        id: 4,
         name: 'Mu App',
         description: 'This project is all about building a platform like Tinder, but for connecting people studying different tech subjects. It helps users find nearby study groups, making it easier to collaborate and network. Created for the Mulearn community, this platform encourages a friendly space where tech enthusiasts can share knowledge and resources, enhancing their learning experience together.',
         tools: ['Flutter', 'AWS', "Google Maps"],
@@ -24,7 +43,7 @@ export const projectsData = [
         image: travel,
     },
     {
-        id: 3,
+        id: 5,
         name: 'Mulearn Backend',
         description: 'This project is a open-source project for the Mulearn community focuses on creating a backend API. It aims to provide essential functionality and support for various applications within the community.',
         tools: ['Django', 'MySql', 'JWT'],
@@ -34,7 +53,7 @@ export const projectsData = [
         image: realEstate,
     },
     {
-        id: 4,
+        id: 6,
         name: 'Smart Classroom',
         description: 'This project involves creating a smart classroom system using Flutter and NodeMCU. It enables users to control classroom lights and fans through a mobile app, allowing for automated adjustments to save power',
         tools: ['Arduino IDE', 'Flutter', 'Firebase'],
